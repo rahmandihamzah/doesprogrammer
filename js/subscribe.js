db7a361b045ce9650a8ed9c4421d708661ddb63a
ranggawdoes <ranggawdoes@gmail.com>
2019-03-17 21:16:18 +0700

    init

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,6 +1,6 @@
 //  Block input number -, +, e
 
-var inputNumber = document.getElementById("number");
+var inputNumber = document.getElementById("phone");
 
 var invalidChars  = [
     "-",
@@ -14,11 +14,43 @@ inputNumber.addEventListener("keydown", function (e) {
     }
 })
 
+// GET INPUT
 
-// My Form Subscribe
+function onSubscribe(event) {
+    event.preventDefault();
 
-var form = document.getElementById("myForm");
+    const url = 'http://192.168.2.12:3000/v1/user'
 
-form.onsubmit = function(){
-    console.log(form.name)
+
+    var inputNama = document.getElementById("name").value;
+
+    var inputEmail = document.getElementById("email").value;
+
+    var inputPhone = document.getElementById("phone").value;
+
+    var subscribeData = {
+        name: inputNama,
+        email: inputEmail,
+        phone: inputPhone
+    }
+
+    var data = JSON.stringify(subscribeData)
+
+    fetch(url, {
+        method: "POST",
+        credentials: 'same-origin',
+        body: data,
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            "Content-Type": "application/json",
+            "Access-Control-Request-Headers": "*",
+            "Access-Control-Request-Method": "*"
+        },
+        mode: "no-cors"
+    }).then(res => {
+        console.log(res)
+    })
+
+
+    console.log(JSON.stringify( subscribeData ));
 }

7d72a00672e5450e42f80e1918ff717c0d26e03f
Rahmandi Hamzah <rahmandi.dev@gmail.com>
2019-03-24 11:49:49 +0700

    form_voucher

--- js/voucher.js
+++ js/voucher.js
@@ -3,51 +3,17 @@ fetch("http://192.168.2.12:3000/v1/vouchers").then(res => {
     response.then(vouchers => {
         console.log(vouchers)
         render(vouchers)
-        // receiver(vouchers)
-        // amount(vouchers)
     })
 }).catch(error => {
     console.log(error)
 })
 
-// function render(vouchers) {
-//     receiver(vouchers)
-//     amount(vouchers)
-
-//     // for (var i = 0; i < vouchers.length; i++) {
-//     //     console.log(vouchers[i])
-
-//     //     document.getElementById('receivers').innerHTML +=
-//     //         "<p class='receiver m-0'>" + vouchers[i].receiver + "</p>";
-
-//     //     document.getElementById('amount').innerHTML =
-//     //         "<p class='amount p-0'>" + vouchers[i].amount + "</p>";
-//     // }
-
-//     function receiver(vouchers) {
-//         for (var i = 0; i < vouchers.length; i++) {
-//             console.log(vouchers[i])
-
-//             document.getElementById('receivers').innerHTML =
-//                 "<p class='receiver m-0'>" + vouchers[i].receiver + "</p>"
-//         }
-//     }
-
-//     function amount(vouchers) {
-//         for (var i = 0; i < vouchers.length; i++) {
-//             document.getElementById('amount').innerHTML =
-//                 "<p class='amount p-0'>" + vouchers[i].amount + "</p>"
-//         }
-
-//     }
-// }
-
-console.log(getQueryStringValue("name"));
+console.log(getQueryStringValue("id"));
 
 function render(vouchers) {
     console.log(vouchers)
     for (let i = 0; i < vouchers.length; i++) {
-        if (vouchers[i].name == getQueryStringValue("name")) {
+        if (vouchers[i]._id == getQueryStringValue("id")) {
 
             document.getElementById('receivers').innerHTML =
                 "<p class='receiver m-0'>" + vouchers[0].name + "</p>"
@@ -60,24 +26,6 @@ function render(vouchers) {
         }
     }
 }
-
-// function receiver(vouchers) {
-//     for (var i = 0; i < vouchers.length; i++) {
-//         console.log(vouchers[i].receiver)
-
-//         document.getElementById('receivers').innerHTML =
-//             "<p class='receiver m-0'>" + vouchers[i].receiver + "</p>"
-//     }
-// }
-
-// function amount(vouchers) {
-//     for (var i = 0; i < vouchers.length; i++) {
-//         console.log(vouchers[i].amount)
-
-//         document.getElementById('amount').innerHTML =
-//             "<p class='amount p-0'>" + vouchers[i].amount + "</p>"
-//     }
-// }
 
 function getQueryStringValue(key) {
     return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));

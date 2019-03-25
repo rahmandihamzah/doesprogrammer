5e15c72de5ed8294a37ab45b3701ae2569de6cfc
Rahmandi Hamzah <rahmandi.dev@gmail.com>
2019-03-25 14:05:31 +0700

    form voucher

--- js/form_voucher.js
+++ js/form_voucher.js
@@ -1,8 +1,8 @@
 fetch("http://192.168.2.12:3000/v1/users").then(res => {
     var response = res.json()
-    response.then(users => {
-        console.log(users)
-        render(users)
+    response.then(data => {
+        console.log(data)
+        render(data.users)
     })
 }).catch(error => {
     console.log(error)
@@ -13,9 +13,12 @@ console.log(getQueryStringValue("id"));
 function render(users) {
     console.log(users)
     for (let i = 0; i < users.length; i++) {
+        console.log(users[i]._id)
         if (users[i]._id == getQueryStringValue("id")) {
 
-            document.getElementById("receiver").innerHTML = "<p class='receiver m-0'>" + users[i].name + "</p>"
+
+            document.getElementById('receiver').innerHTML =
+                "<p class='receiver m-0'>" + users[i].name + "</p>"
         }
     }
 }
